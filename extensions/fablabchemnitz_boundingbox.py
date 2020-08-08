fc960bdc83646a68cffef94f040dae52b6a0eb61
Mario Voigt <mariovoigt89@t-online.de>
2020-08-08 11:54:38 +0200

    Modded bounding box tool to have custom offset values

--- extensions/fablabchemnitz_boundingbox.py
+++ extensions/fablabchemnitz_boundingbox.py
@@ -6,6 +6,7 @@ from lxml import etree
 class DrawBBoxes(inkex.Effect):
     def __init__(self):
         inkex.Effect.__init__(self)
+        self.arg_parser.add_argument('--offset', type=float, default=0.0, help='Offset from object (all directions)')
 
     def effect(self):
         if len(self.svg.selected) > 0:
@@ -13,10 +14,10 @@ class DrawBBoxes(inkex.Effect):
             for id, node, bbox in bboxes:
                 attribs = {
                     'style'     : str(inkex.Style({'stroke':'#ff0000','stroke-width'  : '1','fill':'none'})),
-                    'x'         : str(bbox.left),
-                    'y'         : str(bbox.top),
-                    'width'     : str(bbox.width),
-                    'height'    : str(bbox.height),
+                    'x'         : str(bbox.left - self.options.offset),
+                    'y'         : str(bbox.top - self.options.offset),
+                    'width'     : str(bbox.width + 2 * self.options.offset),
+                    'height'    : str(bbox.height + 2 * self.options.offset),
                 }
                 etree.SubElement(self.svg.get_current_layer(), inkex.addNS('rect','svg'), attribs )
 
